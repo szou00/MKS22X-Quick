06821acd2ca10964e8bd4353819b93ee2962f83e
szou00 <szou00@stuy.edu>
2019-03-13 10:47:50 -0400

    i worked on this at home but didn't push ;( rewrote the code to quickselect

---
+++ Quick.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+
+public class Quick {
+
+  /*return the value that is the kth smallest value of the array.
+ */
+ public static int quickselect(int []data, int k){
+   int p = partition(data,0,data.length-1);
+   System.out.println(p);
+   while (k!=p) {
+     if (p < k) {
+       p = partition(data,p+1,data.length-1);
+       System.out.println(p);
+     }
+     if (p > k) {
+       p = partition(data,0,p-1);
+       System.out.println(p);
+     }
+   }
+   return data[p];
+ }
+
+ public static int partition ( int [] data, int start, int end){
+   Random rand = new Random();
+   int s = start;
+   int x = rand.nextInt(end+1-start) + start;
+   int target = data[x];
+   data[x] = data[start];
+   data[start] = target;
+
+   if (start!= end) {
+     start+=1;
+   }
+   while (start != end) {
+     if (data[start] >= target) {
+       int temp = data[end];
+       data[end] = data[start];
+       data[start] = temp;
+       end-=1;
+     }
+     else {
+       start+=1;
+     }
+   }
+   if (data[start] > target) {
+     data[s] = data[start-1];
+     data[start-1] = target;
+     start -= 1;
+   }
+   else {
+     data[s] = data[start];
+     data[start] = target;
+   }
+   return start;
+ }
+
+
+ public static void main(String args[]) {
+   int[]ary = { 2, 10, 15, 23, 0,  5};
+   // for (int i = 0; i < ary.length; i++) {
+   //   System.out.println(quickselect(ary,i));
+   // }
+   System.out.println(quickselect(ary,4));
+ }
+
+
+}
