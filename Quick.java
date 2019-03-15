87b21ffc529ca3d089f32da40ce51c6b021a4c36
Sharon Zou <szou00@stuy.edu>
2019-03-15 07:26:37 -0400

    comments added

--- Quick.java
+++ Quick.java
@@ -7,16 +7,12 @@ public class Quick {
  */
  public static int quickselect(int []data, int k){
    int p = partition(data,0,data.length-1); //partitions it once
-   while (k!=p) { 
-     if (p < k) {
-       p = partition(data,p+1,data.length-1);
-       // System.out.println(p);
+   while (k!=p) { //while it's not the index that's wanted
+     if (p < k) { //if it's lower than the index wanted
+       p = partition(data,p+1,data.length-1); //look through the values above current index
      }
-     else {
-       if (p > k) {
-         p = partition(data,0,p-1);
-         // System.out.println("Second loop: " + p);
-       }
+     if (p > k) { //if it's lower than index that's wanted
+       p = partition(data,0,p-1); //look through the values below current index
      }
    }
    return data[p];
@@ -28,11 +24,12 @@ public class Quick {
      quicksort(data,0,data.length-1);
    }
 
+   //helper method
    public static void quicksort(int[] data, int lo, int hi) {
      if (lo < hi) { //the only way something would happen
-       int pivot = partition(data,lo,hi);
-       quicksort(data,pivot+1,hi);
-       quicksort(data,lo,pivot-1);
+       int pivot = partition(data,lo,hi); //partitions it once
+       quicksort(data,pivot+1,hi); //calls itself to partition the two different parts of the array
+       quicksort(data,lo,pivot-1); //below the index and above the index
      }
    }
 
@@ -41,60 +38,62 @@ public class Quick {
  public static int partition ( int [] data, int start, int end){
    //keeping track of the original start
    int s = start;
-   // System.out.println(end+1-start);
 
    //choosing the median of the lowest, highest, and middle elements
    int med = 0;
+   //the middle value is the median
    if (data[(start+end)/2] <= data[start] && data[(start+end)/2] >= data[end] || data[(start+end)/2] >= data[start] && data[(start+end)/2] <= data[end]) {
      med = (start+end)/2;
    }
+   //the end value is the median
    if (data[end] >= data[start] && data[end] <= data[(start+end)/2] || data[end] <= data[start] && data[end] >= data[(start+end)/2]) {
      med = end;
    }
+   //the first value is the median
    if (data[start] <= data[(start+end)/2] && data[start] >= data[end] || data[start] <= data[(start+end)/2] || data[start] >= data[(start+end)/2]) {
      med = start;
    }
-   // System.out.println(min + " " +  med + " " + max);
-   int x = med;
+
    //swapping target to the front
-   int target = data[x];
-   data[x] = data[start];
-   data[start] = target;
+   int target = data[med]; //target stores the median
+   data[med] = data[start]; //the index of median gets the value that's at the beginning
+   data[start] = target; //the beginning gets the target value
 
    //swapping in general
    if (start!= end) {
      start+=1;
    }
-   while (start != end) {
-     int equal = -1;
-     if (data[start] == target) {
+   while (start != end) { //while start is not equal to end
+     int equal = -1; //initializes this
+     if (data[start] == target) { //if it's equal
        Random r = new Random();
-       equal = r.nextInt(2);
+       equal = r.nextInt(2); //randomly chooses to go to the front or back
      }
-     if (data[start] > target || equal == 0) {
-       int temp = data[end];
+     if (data[start] > target || equal == 0) { //if it's greater than target or is equal to 0
+       int temp = data[end]; //swaps to the end
        data[end] = data[start];
        data[start] = temp;
        end-=1;
      }
      else {
-       start+=1;
+       start+=1; //otherwise start is incremented
      }
    }
 
-   if (data[start] > target) {
-     data[s] = data[start-1];
+   //swapping pivot back
+   if (data[start] > target) { //if the current value is greater than target
+     data[s] = data[start-1]; //swap target back with the value in front of the current value
      data[start-1] = target;
      start -= 1;
    }
    else {
-     data[s] = data[start];
+     data[s] = data[start]; //otherwise just swaps it with the current value
      data[start] = target;
    }
    return start;
  }
 
-
+//testing from k's website
  public static void main(String[]args){
   System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
   int[]MAX_LIST = {1000000000,500,10};
